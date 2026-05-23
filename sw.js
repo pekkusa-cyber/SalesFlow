ee765032da25853978f2f63aac862f9b3a9e81a1
pekkusa-cyber <pekkusa@gmail.com>
2026-05-23 22:54:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Krävs för PWA-installation, lämnas öppen för nätverksanrop så att Supabase fungerar direkt
+});
